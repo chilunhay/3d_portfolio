fc1aad8dd9f39ff41c9d77de84fea4e3401ff262
chilunhay <72005042+chilunhay@users.noreply.github.com>
2026-05-29 09:53:57 +0700

    fix link error

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,6 @@
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
+import PropTypes from "prop-types";
 
 import { styles } from "../styles";
 import { github } from "../assets";
@@ -15,55 +16,71 @@ const ProjectCard = ({
   image,
   deploy_link,
   source_code_link,
-}) => {
-  return (
-    <motion.div variants={fadeIn("up", "string", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-      >
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt={name}
-            className="w-full h-full object-cover rounded-2xl"
-          />
+}) => (
+  <motion.div variants={fadeIn("up", "string", index * 0.5, 0.75)}>
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+    >
+      <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+        <div
+          className="absolute inset-0 flex justify-end m-3 card-img_hover cursor-pointer"
+          onClick={() => window.open(deploy_link, "_blank")}
+        >
           <div
-            className="absolute inset-0 flex justify-end m-3 card-img_hover cursor-pointer"
-            onClick={() => window.open(deploy_link, "_blank")}
+            onClick={(e) => {
+              e.stopPropagation();
+              window.open(source_code_link, "_blank");
+            }}
+            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
           >
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={github}
-                alt="source code"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
+            <img
+              src={github}
+              alt="source code"
+              className="w-1/2 h-1/2 object-contain"
+            />
           </div>
         </div>
+      </div>
 
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
+      <div className="mt-5">
+        <h3 className="text-white font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-      </Tilt>
-    </motion.div>
-  );
+      <div className="mt-4 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>
+        ))}
+      </div>
+    </Tilt>
+  </motion.div>
+);
+
+ProjectCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  image: PropTypes.string.isRequired,
+  deploy_link: PropTypes.string.isRequired,
+  source_code_link: PropTypes.string.isRequired,
 };
 
 const Works = () => {
